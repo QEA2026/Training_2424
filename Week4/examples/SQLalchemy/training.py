e80d7784442cafe3e4739881755e26a6ce092217
WilliamTRev <william.terry@revature.com>
2026-06-22 11:46:57 -0400

    added SQLAlchemy with new record

--- Week4/examples/SQLalchemy/training.py
+++ Week4/examples/SQLalchemy/training.py
@@ -2,7 +2,7 @@ from os import getenv
 
 from dotenv import load_dotenv
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 
 load_dotenv()
@@ -19,3 +19,44 @@ df.to_sql(
     if_exists = 'replace',
     index=False
 )
+
+#Get input from the user
+first_name = input ("First name: ")
+last_name = input ("Last name: ")
+email = input ("email: ")
+hire_date = input ("Hire Date (YYYY-MM-DD): ")
+salary = float(input("Salary: "))
+
+with engine.begin() as conn:
+    result = conn.execute(
+        text("""
+            INSERT INTO employees (
+             first_name,
+             last_name,
+             email,
+             hire_date,
+             salary
+             )
+             VALUES (
+             :first_name,
+             :last_name,
+             :email,
+             :hire_date,
+             :salary
+             )
+             RETURNING employee_id
+             """),
+             {
+                 "first_name": first_name,
+                 "last_name": last_name,
+                 "email": email,
+                 "hire_date": hire_date,
+                 "salary": salary
+             }
+    )
+    employee_id = result.scalar()
+    print(f"Employee {employee_id} inserted successfully!")
+
+
+    
+
