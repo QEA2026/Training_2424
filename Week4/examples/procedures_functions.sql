7f2de0e4bd845b76d923b6849b846c209ea04d79
WilliamTRev <william.terry@revature.com>
2026-06-24 10:28:42 -0400

    procedures and functions data added

--- Week4/examples/procedures_functions.sql
+++ Week4/examples/procedures_functions.sql
@@ -12,8 +12,25 @@ CREATE TABLE customer (
 
 CREATE TABLE orders (
 	order_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	customer_id INT NOT NULL REFERENCES customer(customer_id),
+	customer_id INT NOT NULL REFERENCES customer(customer_id) ON DELETE CASCADE,
 	order_date DATE NOT NULL DEFAULT CURRENT_DATE,
 	total_amount NUMERIC(10,2) NOT NULL CHECK (total_amount>0)
 );
 
+INSERT INTO customer(customer_name)
+VALUES
+('Alice Johnson'),
+('Bob Smith'),
+('Carol Davis');
+
+INSERT INTO orders(customer_id, total_amount)
+VALUES
+(1,2500.00),
+(1, 3500.00),
+(2,5000.00),
+(2,7500.00),
+(3,300.00),
+(3,300.00);
+
+SELECT * FROM orders;
+SELECT * FROM customer;
