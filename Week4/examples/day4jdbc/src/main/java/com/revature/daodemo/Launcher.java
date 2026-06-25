05a893d3b3468b26abb4d7739ddc681e73d95060
WilliamTRev <william.terry@revature.com>
2026-06-25 11:46:08 -0400

    dao demo started

--- Week4/examples/day4jdbc/src/main/java/com/revature/daodemo/Launcher.java
+++ Week4/examples/day4jdbc/src/main/java/com/revature/daodemo/Launcher.java
@@ -1,4 +1,32 @@
 package com.revature.daodemo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Launcher {
+    public static void main(String[] args) throws SQLException
+            {
+        String url = "jdbc:sqlite:week4_jdbc_dao_demo.db";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+
+            //open database connection
+            try (Statement st = conn.createStatement()) {
+
+                st.executeUpdate("DROP TABLE IF EXISTS product");
+
+                st.executeUpdate("""
+                    CREATE TABLE product (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        sku TEXT NOT NULL UNIQUE,
+                        name TEXT NOT NULL,
+                        price REAL NOT NULL
+                    )
+
+                    """);
+            }
+        }
+    }
 }
