70faef80d34c1ee62d0813b2dca925b70e59c9e2
WilliamTRev <william.terry@revature.com>
2026-06-30 11:42:19 -0400

    added coverage example

--- Week5/examples/day2/CoverageExample/test_shipping.py
+++ Week5/examples/day2/CoverageExample/test_shipping.py
@@ -10,7 +10,8 @@ def test_standard_returned_for_large_non_priority_order():
 def test_no_free_shipping_returned_for_small_order():
     assert get_shipping_tier(50.00, priority=False) == "NO_FREE_SHIPPING"
 
-def test_wrong_oracle_still_100_percent():
-    result = get_shipping_tier(150.00,priority=True)
-    # assert result == "EXPRESS"
-    assert result == "STANDARD"
+# 100 percent coverage does not imply 100 percent accurate expected values
+# def test_wrong_oracle_still_100_percent():
+#     result = get_shipping_tier(150.00,priority=True)
+#     # assert result == "EXPRESS"
+#     assert result == "STANDARD"
