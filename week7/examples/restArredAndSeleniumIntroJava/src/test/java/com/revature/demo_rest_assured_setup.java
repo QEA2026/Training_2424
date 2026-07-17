eb919a3457c01581dc90f0fa77bc6bdd67be58a7
WilliamTRev <william.terry@revature.com>
2026-07-17 10:46:11 -0400

    first rest assured test

--- week7/examples/restArredAndSeleniumIntroJava/src/test/java/com/revature/demo_rest_assured_setup.java
+++ week7/examples/restArredAndSeleniumIntroJava/src/test/java/com/revature/demo_rest_assured_setup.java
@@ -6,6 +6,9 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 
 @DisplayName("REST Assured Setup Demo")
 public class demo_rest_assured_setup {
@@ -28,6 +31,20 @@ public class demo_rest_assured_setup {
     static void teardown(){
         //Reset REST Assured to Defaults
         RestAssured.reset();
+    }
+
+    //First REST Assured Test
+    @Test
+    @DisplayName("First REST Assured Test - GET a post")
+    void firstTest_getPost_returnSuccessfully() {
+
+        given() //Setup
+            .log().all() //Log the request (optional)
+        .when() //Action
+            .get("/posts/1")
+        .then() //Validation
+            .log().all() //log the response
+            .statusCode(200);
     }
 
 
