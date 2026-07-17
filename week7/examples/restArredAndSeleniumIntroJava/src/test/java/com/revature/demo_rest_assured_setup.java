1c88e2481c13fe7178fcbd0aec92aa99103f5768
WilliamTRev <william.terry@revature.com>
2026-07-17 10:51:11 -0400

    more detailed request

--- week7/examples/restArredAndSeleniumIntroJava/src/test/java/com/revature/demo_rest_assured_setup.java
+++ week7/examples/restArredAndSeleniumIntroJava/src/test/java/com/revature/demo_rest_assured_setup.java
@@ -3,12 +3,14 @@ package com.revature;
 //REST assured Setup and First Tests
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
 
 @DisplayName("REST Assured Setup Demo")
 public class demo_rest_assured_setup {
@@ -45,6 +47,32 @@ public class demo_rest_assured_setup {
         .then() //Validation
             .log().all() //log the response
             .statusCode(200);
+    }
+
+    //Given-When-Then Breakdown
+
+    @Test
+    @DisplayName("Detailed Given-When-Then example")
+    void givenWhenThen_detailed_breakdown(){
+
+        given()
+            //Headers
+            .header("Accept","application/json")
+            //Query Parameters
+            .queryParam("userId",1)
+
+        .when()
+            //HTTP method +endpoint
+            .get("/posts")
+
+        .then()
+            //status code validation
+            .statusCode(200)
+            //Content-Type validation
+            .contentType(ContentType.JSON)
+            //Body validation using Hamcrest matchers
+            .body("size()",greaterThan(0));
+
     }
 
 
